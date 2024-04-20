be3c36500efe5bac0e022eabe165f9dc93300998
James Milne <jamesmilne925@gmail.com>
2024-04-20 17:28:39 -0400

    completed beat map v1

--- road.gd
+++ road.gd
@@ -3,48 +3,35 @@ extends Node2D
 @onready var note_conductor = $NoteConductor
 var note_scene = preload("res://note.tscn")
 
-var notes = []
-var note_length = -500
-var current_location = Vector2(0, note_length)
 var note_distance_scale = .25
+var note_count = 0
+var beat_map_file_name = "res://beatMap1.txt"
 
 var note_move_speed = 3
 
-#TODO: Need to make unit scaling more consistent
-var notes_data = [
-	{
-		"pos": 0,
-		#"len": 100
-	},
-	{
-		"pos": 400,
-		#"len": 100
-	},
-	{
-		"pos": 800,
-		#"len": 100
-	},
-	{
-		"pos": 1200,
-		#"len": 100
-	},
-]
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for i in range(2):
-		add_note(i)
+
+	var file = FileAccess.open(beat_map_file_name, FileAccess.READ)
+	var beat_map_raw = file.get_as_text()
+	var beat_map = beat_map_raw.split('\n')
+	
+	for i in range(len(beat_map)):
+		add_note(beat_map[i].erase(len(beat_map[i])-1, 1))
 
 func _process(delta):
 	#print(bar_conductor)
 	note_conductor.position += Vector2(0 , note_move_speed)
 
-func add_note(note_index):
+func add_note(note_lane):
+
+	if note_lane == "-":
+		note_count += 1
+		return
 	var note = note_scene.instantiate()
-	# -- Note position are randomized for now for testing
-	randomize()
-	note.line = randi_range(0,2)
-	# --
-	note.note_position = note_index * note_distance_scale
+
+	note.line = note_lane
+
+	note.note_position = note_count * 100
 	note_conductor.add_child(note)
-	#print(note.line)
+	note_count += 1
