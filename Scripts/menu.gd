687e40a9dbfc2224e3ff1d6a54fb02db91344478
James Milne <jamesmilne925@gmail.com>
2024-04-21 16:47:47 -0400

    color changes

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -13,3 +13,16 @@ func _process(delta):
 
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://Scene/guts.tscn")
+
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+
+func _on_credits_pressed():
+	#var credits = load().instance()
+	pass
+
+
+func _on_settings_pressed():
+	pass # Replace with function body.
