4a78a9d69372e3481d038b0749abcc23559a7022
James Milne <jamesmilne925@gmail.com>
2024-04-21 15:47:12 -0400

    added multi note to beat map

--- road.gd
+++ road.gd
@@ -1,46 +1,62 @@
 extends Node2D
 
 @onready var note_conductor = $NoteConductor
+var breakable_flesh = preload("res://Scene/breakable_flesh.tscn")
 var note_scene = preload("res://note.tscn")
 
-var note_count = 0
+var row_count = 0
 var beat_map_file_name = "res://beatMap1.txt"
 var note_move_speed
-var note_scale
+var bpm
+
 
 const DIST_BETWEEN_NOTES = 200
+
+func parse_beat_map(file_name):
+	var file = FileAccess.open(file_name, FileAccess.READ)
+	var beat_map_raw = file.get_as_text()
+	var beat_map = beat_map_raw.split('\n')
+	var out = []
+	
+	bpm = beat_map[0]
+	
+	for i in range(1, len(beat_map)):
+		var vals = beat_map[i].split(',')
+		out.append(vals)
+	return out
+		
 
 func calculate_note_speed(bpm):
 	var note_length = DIST_BETWEEN_NOTES
 	var beat_in_seconds = 60/float(bpm)
-	
 	note_move_speed = note_length/beat_in_seconds
-	note_scale = note_length/DIST_BETWEEN_NOTES
 
 func _ready():
 
-	var file = FileAccess.open(beat_map_file_name, FileAccess.READ)
-	var beat_map_raw = file.get_as_text()
-	var beat_map = beat_map_raw.split('\n')
-	var bpm = 83
+	var beat_map = parse_beat_map(beat_map_file_name)
 
 	calculate_note_speed(bpm)
 	
 	for i in range(len(beat_map)):
-		add_note(beat_map[i])
+		add_row(beat_map[i])
 
 func _process(delta):
 	note_conductor.position += Vector2(-note_move_speed*delta, 0)
 
-func add_note(note_lane):
+func add_row(row):
+	for i in range(len(row)):
+		add_note(row[i], i)
+	row_count += 1
 
-	if note_lane == "-":
-		note_count += 1
+func add_note(note_type, note_lane):
+	var note
+	if note_type == "-":
 		return
-	var note = note_scene.instantiate()
+	elif note_type == "bf":
+		note = breakable_flesh.instantiate()
+	else:
+		note = note_scene.instantiate()
 
 	note.line = note_lane
-
-	note.note_position = note_count * DIST_BETWEEN_NOTES
+	note.note_position = row_count * DIST_BETWEEN_NOTES
 	note_conductor.add_child(note)
-	note_count += 1
